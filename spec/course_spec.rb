d70434b256762a0f77d7d5199a094b5b1b4713ab
pcbennett108 <pcbennett42@gmail.com>
2023-05-24 09:39:14 -0600

    Add tests for Iteration_2, Course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,33 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})
+  end
+
+  it "exists" do
+    expect(@course).to be_instance_of(Course)
+  end
+
+  it "has attributes" do
+    expect(@course.name).to eq("Calculus")
+    expect(@course.capacity).to eq(2)
+  end
+
+  it "starts with no students" do
+    expect(@course.students).to eq([])
+  end
+
+  it "can check if it's full" do
+    expect(@course.full?).to be(false)
+  end
+
+  it "can enroll students" do
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.full?).to be(true)
+  end
 
 end
