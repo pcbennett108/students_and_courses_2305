ddf1a7b5bd5e8df9980c96b2633642281ecbf178
pcbennett108 <pcbennett42@gmail.com>
2023-05-24 09:13:11 -0600

    Add tests for Iteration_1, Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,30 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+
+  it "exists" do
+    expect(@student).to be_instance_of(Student)
+  end
+
+  it "has attributes" do
+    expect(@student.name).to eq("Morgan")
+    expect(@student.age).to eq(21)
+    expect(@student.scores).to eq([])
+  end
   
+  it "keeps scores" do
+    student.log_score(89)
+    student.log_score(78)
+    expect(@student.scores).to eq([89, 78])
+  end
+
+  it "can give student grade average of scores" do
+    student.log_score(89)
+    student.log_score(78)
+    expect(@student.scores).to eq(83.5)
+  end
+
 end
